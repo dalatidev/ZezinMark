0ef1b549093a6abe67844142bf88ab67bfd1a09a
Dalati Lacerda <dalati.dev@gmail.com>
2026-08-21 10:21:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -528,7 +528,7 @@ function showFinalResult() {
   );
   addMsg(
     "bot",
-    `Veja mais projetos no meu site: <a href="dalatidev.com.br" target="_blank" class="prime-btn" style="margin-top:8px; text-decoration:none;">dalatidev.com.br <i data-lucide="instagram" size="16"></i></a>`
+    `Veja mais projetos no meu site: <a href="https://dalatidev.com.br" target="_blank" class="prime-btn" style="margin-top:8px; text-decoration:none;">dalatidev.com.br <i data-lucide="instagram" size="16"></i></a>`
   );
 
   lucide.createIcons();
